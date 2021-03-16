39ad87790c595372d6092c3947df28dd3e56f78d
apkoha <79258179+apkoha@users.noreply.github.com>
2021-03-16 22:38:01 +0300

    доступность

--- js/credit.js
+++ js/credit.js
@@ -3,13 +3,11 @@ var creditPopup = document.querySelector(".credit-block");
 var creditClose = document.querySelector(".warranty-button");
 
 creditButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
   creditPopup.classList.add("credit-block-show"); /*показывает блок Кредит*/
   deliveryPopup.classList.remove("delivery-block-show"); /*убирает блок Доставка*/
   warrantyPopup.classList.remove("warranty-block-show");  /*убирает блок Гарантия*/
 });
 
 creditClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
   creditPopup.classList.remove("credit-block-show"); /*убирает блок Кредит*/
 });
--- js/delivery.js
+++ js/delivery.js
@@ -3,13 +3,11 @@ var deliveryPopup = document.querySelector(".delivery-block");
 var deliveryClose = document.querySelector(".warranty-button");
 
 deliveryButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
   deliveryPopup.classList.add("delivery-block-show"); /*показывает блок Доставка*/
   warrantyPopup.classList.remove("warranty-block-show"); /*убирает блок Гарантия*/
   creditPopup.classList.remove("credit-block-show"); /*убирает блок Кредит*/
 });
 
 deliveryClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
   deliveryPopup.classList.remove("delivery-block-show"); /*убирает блок Доставка*/
 });
